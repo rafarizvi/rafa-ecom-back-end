86b8c37fc43b577c616fdcfb96e620cc3251b3e8
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-04-22 07:52:09 -0400

    categories also display the products, products display the tags

--- models/index.js
+++ models/index.js
@@ -5,12 +5,35 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_Key: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  // Defines a third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    foreign_Key: 'tag_id',
+    onDelete: 'CASCADE'
+  },
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Defines a third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    // unique: false
+  },
+  as: 'tag_id'
+})
 
 module.exports = {
   Product,
