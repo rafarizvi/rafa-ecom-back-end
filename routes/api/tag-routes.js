8a9bdbc7c39afd9c9dc43739b8fcc3cf5052ce31
rafarizvi <152836959+rafarizvi@users.noreply.github.com>
2024-04-23 00:12:22 -0400

    added delete route to tags

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -52,7 +52,7 @@ router.put('/:id', async (req, res) => {
   try {
     const tagData = await Tag.update(req.body, {
       where: {
-        id: req.params.id,
+        id: req.params.id
       }
     })
     if (!tagData[0]) {
@@ -65,8 +65,22 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(400).json({ message: 'No tag found with this id' })
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
